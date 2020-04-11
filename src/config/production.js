9d78db10e073bdfc79ed1a974e8faf78427d0bd6
Vinit Kumar <vinit928@gmail.com>
2020-04-11 19:31:52 +0530

    updated

--- src/config/production.js
+++ src/config/production.js
@@ -1,6 +1,6 @@
 const productionConfig = {
   isDev: false,
-  DB_URL: 'mongodb://airbuk:qwerty1234@ds031319.mlab.com:31319',
+  DB_URL: 'mongodb://airbuk:1234qwerasd@54.36.173.197:27017',
   DB_NAME: 'airbuk',
   TOKEN_SECRET: 'TOKEN_SECRET',
   TOKEN_OPTIONS: {
@@ -10,6 +10,7 @@ const productionConfig = {
   REFRESH_TOKEN_OPTIONS: {
     expiresIn: '7d',
   },
+  image_url: "http://api.airbuk.com/image/"
 };
 
 productionConfig.DB_URI = `${productionConfig.DB_URL}/${productionConfig.DB_NAME}`;
