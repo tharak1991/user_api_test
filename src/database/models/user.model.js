6bb66cd09d933d7fa3a6e956c64b333b2184fbf0
Vinit Kumar <vinit928@gmail.com>
2020-04-20 22:14:13 +0530

    added

--- src/database/models/user.model.js
+++ src/database/models/user.model.js
@@ -5,11 +5,11 @@ const userSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     phone: {type: String},
-    image: {type: String},
     password: {type: String, required: true},
     username: {type: String, required: true},
     dob: {type: Date},
-    addedDate: {type: Date, required: true, default: new Date()}
+    addedDate: {type: Date, required: true, default: new Date()},
+    lastActive: {type: Date, required: true, default: new Date()},
 });
 
 module.exports = mongoose.model('user', userSchema, 'user');
