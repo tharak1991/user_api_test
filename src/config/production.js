5de4c5c9cd43f1673d0d7b50a6409a4847f8b641
Vinit Kumar <vinit928@gmail.com>
2020-04-25 12:50:58 +0530

    mongo atles added

--- src/config/production.js
+++ src/config/production.js
@@ -1,8 +1,8 @@
 const productionConfig = {
   isDev: false,
   // DB_URL: 'mongodb://airbuk:1234qwerasd@54.36.173.197:27017',
-  DB_URL: 'mongodb://airbuk:qwerty1234@ds031319.mlab.com:31319',
-  DB_NAME: 'airbuk',
+  DB_URL: 'mongodb+srv://airbuk:qwerty1234@cluster0-tzqlu.mongodb.net',
+  DB_NAME: 'test?retryWrites=true&w=majority',
   TOKEN_SECRET: 'TOKEN_SECRET',
   TOKEN_OPTIONS: {
     expiresIn: '24h',
