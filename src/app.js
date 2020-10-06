16c3a3344e0b9630ef8b8a887a2104f0643ee975
THARAK <tharak1991@gmail.com>
2020-10-06 18:18:25 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const compression = require('compression');
@@ -15,21 +14,24 @@ app.use(bodyParser.json({
     limit: '50mb',
     extended: true,
 }));
+
 app.use(bodyParser.urlencoded({
     extended: false,
     parameterLimit: 100000,
     limit: '50mb',
 }));
+
 app.use(compression());
 app.use(cookieParser());
 app.use(cors());
 app.use(morgan('dev'));
 
+
+
 app.use('/user/', user_route);
 app.use('/employee/', employee_route);
 
 
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use('*', (req, res) => {
     res.sendStatus(404);
--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -1,35 +1,20 @@
 const express = require('express');
 const routes = express.Router();
-const multer  = require('multer');
-const upload = multer({dest:'uploads/'});
+const multer = require('multer');
+const upload = multer({
+    dest: 'uploads/'
+});
+const fs = require('fs');
 
 const employee_controller = require('../controllers/employee.controller');
 
 
+const path = require('path');
 
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads');
-//     },
-//     filename: (req, file, cb) => {
-//         console.log(file);
-//         cb(null, Date.now() + path.extname(file.originalname));
-//     }
-// });
-
-// const fileFilter = (req, file, cb) => {
-//     if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
-//         cb(null, true);
-//     } else {
-//         cb(null, false);
-//     }
-// }
-
-// const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 routes.post('/add', async (req, res, next) => {
     try {
-        const emp_req = req.body ;
+        const emp_req = req.body;
         let employee = await employee_controller.save(emp_req);
         // await email_controller.sendOTP(user._id);
         await res.status(201).json({
@@ -46,24 +31,24 @@ routes.post('/add', async (req, res, next) => {
 });
 
 
-routes.post('/single', upload.single('image'), (req, res) => {
+routes.post('/upload', upload.single('image'), (req, res) => {
     try {
-      res.send(req.file);
-    }catch(err) {
-      res.send(400);
+        res.send(req.file);
+    } catch (err) {
+        res.send(400);
     }
+});
+
+
+
+routes.get('/download', function(req, res){
+    try {
+    var file = '/home/tarak/Tarak/Projects/assignments/ent-bot/ent_bot_test/images' + '/test1.jpg';
+    res.download(file);
+}catch (err) {
+    res.send(400);
+}
   });
-
-
-// routes.post('/upload', upload.single('image'), (req, res, next) => {
-//     try {
-//         return res.status(201).json({
-//             message: 'File uploded successfully'
-//         });
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
 
 
 routes.use('*', (req, res) => {
