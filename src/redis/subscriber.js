2471b8a982ec1a0bd4c261a1f14b46e00edefff2
THARAK <tharak1991@gmail.com>
2020-11-08 18:09:05 +0530

    updates

--- src/redis/subscriber.js
+++ src/redis/subscriber.js
@@ -4,7 +4,7 @@ const redis_publisher = require('./publisher');
 
 
 
-exports.subscribeUserInfoReq = async() => {
+exports.subscribeUserInfoReq = async () => {
     const subscriber = redis.createClient();
 
     subscriber.on("message", (channel, message) => {
@@ -12,20 +12,15 @@ exports.subscribeUserInfoReq = async() => {
         const user = JSON.parse(message);
 
         if (user) {
-            const user_info =  user_controller.getById(user.id).then((user_info) =>{
-                redis_publisher.publishUserRequestEvent(user_info);
-                console.log('user_info', user_info );
-
-            }
+            const user_info = user_controller.getById(user.id).then((user_info) => {
+                    redis_publisher.publishUserRequestEvent(user_info);
+                    console.log('user_info', user_info);
+                }
 
             ).catch(error => console.log(error));
-           
-            // if(user_info){
-            //     redis_publisher.publishUserRequestEvent(user_info);
-            // }
-            
+
         }
-})
+    })
 
     subscriber.subscribe("user-notify");
 }
